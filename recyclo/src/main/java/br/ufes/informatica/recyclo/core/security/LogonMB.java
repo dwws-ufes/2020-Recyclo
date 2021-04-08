2e6c7e86349ddc4fc57d3d0f639e20332b1d7eca
LKhoe <leonardokhoury@hotmail.com>
2021-04-08 11:12:53 -0300

    login: init

---
+++ recyclo/src/main/java/br/ufes/informatica/recyclo/core/security/LogonMB.java
@@ -0,0 +1,34 @@
+import javax.enterprise.context.SessionScoped;
+import javax.enterprise.inject.Specializes;
+import com.github.adminfaces.template.session.AdminSession;
+import org.omnifaces.util.Faces;
+import java.io.Serializable;
+import java.io.IOException;
+// import static com.github.adminfaces.starter.util.Utils.addDetailMessage;
+// import br.ufes.informatica.recyclo.core.controller.LoginController;
+
+
+@SessionScoped
+@Specializes
+public class LogonMB extends AdminSession implements Serializable {
+
+    private String currentUser;
+    private String email;
+    private String password;
+    private boolean remember;
+
+    // public void login() throws IOException {
+    //     currentUser = email;
+        
+    //     addDetailMessage("Logged in successfully as <b>" + email + "</b>");
+    //     Faces.getExternalContext().getFlash().setKeepMessages(true);
+    //     Faces.redirect("index.xhtml");
+    // }
+
+    @Override
+    public boolean isLoggedIn() {
+        return false;
+    }
+
+    //getters&setters
+}
--- recyclo/src/main/webapp/index.jsp
+++ recyclo/src/main/webapp/index.jsp
@@ -1,4 +1,7 @@
 <html>
+    <head>
+        <meta http-equiv="refresh" content="0; URL='core/login/login.xhtml'"/>
+    </head>
 <body>
 <h2>Hello, Recyclo!</h2>
 <p><%= new java.util.Date() %></p>
